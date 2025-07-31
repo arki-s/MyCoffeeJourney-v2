f08f61f9ce6a3a2a90fa284a894c2ff622d2409d
exec zsh <wind10cloud@gmail.com>
2025-07-31 15:47:52 +0900

    testing supabase connection

--- App.tsx
+++ App.tsx
@@ -1,7 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { supabase } from './supabase';
 
 export default function App() {
+
+  useEffect(() => {
+  const fetchData = async () => {
+    const { data, error } = await supabase.from('coffee').select('*');
+    if (error) console.error(error);
+    else console.log('Beans:', data);
+  };
+
+  fetchData();
+}, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
