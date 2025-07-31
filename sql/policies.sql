36097f9fbf683f6cb60cb5b6cf98bd05c6a5d13f
exec zsh <wind10cloud@gmail.com>
2025-07-31 15:53:34 +0900

    added sql files

---
+++ sql/policies.sql
@@ -0,0 +1,56 @@
+CREATE POLICY "Users can access own account only"
+ON users
+FOR ALL
+USING (id = auth.uid());
+
+CREATE POLICY "Users can access own coffee only"
+ON coffee
+FOR ALL
+USING (user_id = auth.uid());
+
+CREATE POLICY "Only owner can modify or read coffee brands"
+on coffee_brands
+FOR ALL
+USING (user_id = auth.uid());
+
+CREATE POLICY "Only owner can modify or read coffee beans"
+ON coffee_beans
+FOR ALL
+USING (user_id = auth.uid());
+
+CREATE POLICY "Access only own coffee bean mappings"
+ON coffee_bean_inclusions
+FOR ALL
+USING (
+  EXISTS (
+    SELECT 1 FROM coffee
+    WHERE id = coffee_bean_inclusions.coffee_id
+    AND user_id = auth.uid()
+  )
+);
+
+CREATE POLICY "Only owner can modify or read grind sizes"
+ON grind_sizes
+FOR ALL
+USING (user_id = auth.uid());
+
+CREATE POLICY "Only owner can modify or read drinking records"
+ON drinking_records
+FOR ALL
+USING (user_id = auth.uid());
+
+CREATE POLICY "Access only own coffee bean mappings"
+ON drinking_grind_sizes
+FOR ALL
+USING (
+  EXISTS (
+    SELECT 1 FROM drinking_records
+    WHERE id = drinking_grind_sizes.record_id
+    AND user_id = auth.uid()
+  )
+);
+
+CREATE POLICY "Only owner can modify or read reviews"
+ON reviews
+FOR ALL
+USING (user_id = auth.uid());
---
+++ sql/schema.sql
@@ -0,0 +1,77 @@
+CREATE TABLE users (
+id uuid PRIMARY KEY references auth.users(id) on delete cascade,
+display_name text,
+icon_url text,
+created_at timestamp with time zone default now()
+);
+
+CREATE TABLE coffee (
+id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+name text NOT NULL,
+comments text,
+photo_url text,
+roast_level int NOT NULL DEFAULT 1,
+body int NOT NULL DEFAULT 1,
+sweetness int NOT NULL DEFAULT 1,
+fruity int NOT NULL DEFAULT 1,
+bitter int NOT NULL DEFAULT 1,
+aroma int NOT NULL DEFAULT 1,
+user_id uuid REFERENCES users(id),
+created_at timestamp with time zone DEFAULT now()
+);
+
+CREATE TABLE coffee_brands (
+id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+name text NOT NULL,
+user_id uuid REFERENCES users(id),
+created_at timestamp with time zone DEFAULT now()
+);
+
+CREATE TABLE coffee_beans (
+id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+name text NOT NULL,
+user_id uuid REFERENCES users(id),
+created_at timestamp with time zone DEFAULT now()
+);
+
+CREATE TABLE coffee_bean_inclusions (
+id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+coffee_id uuid REFERENCES coffee(id),
+bean_id uuid REFERENCES coffee_beans(id),
+created_at timestamp with time zone DEFAULT now()
+);
+
+CREATE TABLE grind_sizes (
+id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+name text NOT NULL,
+user_id uuid REFERENCES users(id),
+created_at timestamp with time zone DEFAULT now()
+);
+
+CREATE TABLE drinking_records (
+id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+weight_grams int NOT NULL,
+price_yen int NOT NULL,
+start_date date NOT NULL,
+end_date date,
+purchase_date date,
+coffee_id uuid REFERENCES coffee(id),
+user_id uuid REFERENCES users(id),
+created_at timestamp with time zone DEFAULT now()
+);
+
+CREATE TABLE drinking_grind_sizes (
+id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+grind_size_id uuid REFERENCES grind_sizes(id),
+record_id uuid REFERENCES drinking_records(id),
+created_at timestamp with time zone DEFAULT now()
+);
+
+CREATE TABLE reviews (
+id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+score int NOT NULL,
+comments text,
+record_id uuid REFERENCES drinking_records(id),
+user_id uuid REFERENCES users(id),
+created_at timestamp with time zone DEFAULT now()
+);
