2a0bcceb1bf4fa65d9ac3f0e4eaa9172761bed4f
exec zsh <wind10cloud@gmail.com>
2025-08-01 19:43:36 +0900

    react-dom exorcised, Clerk purged

--- App.tsx
+++ App.tsx
@@ -2,18 +2,20 @@ import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { supabase } from './supabase';
+import * as SecureStore from 'expo-secure-store'
+import * as React from 'react';
 
 export default function App() {
 
   useEffect(() => {
-  const fetchData = async () => {
-    const { data, error } = await supabase.from('coffee').select('*');
-    if (error) console.error(error);
-    else console.log('Beans:', data);
-  };
+    const fetchData = async () => {
+      const { data, error } = await supabase.from('coffee').select('*');
+      if (error) console.error(error);
+      else console.log('Beans:', data);
+    };
 
-  fetchData();
-}, []);
+    fetchData();
+  }, []);
 
   return (
     <View style={styles.container}>
