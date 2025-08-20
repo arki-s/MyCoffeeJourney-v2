49c99b630527f43b64e7c216f957df2175809b0f
exec zsh <wind10cloud@gmail.com>
2025-08-20 17:03:05 +0900

    Installed React Navigation, added screen files

---
+++ features/main/screens/AnalysisScreen.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function AnalysisScreen() {
+  return (
+    <View>
+      <Text>AnalysisScreen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
---
+++ features/main/screens/CoffeeListScreen.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function CoffeeListScreen() {
+  return (
+    <View>
+      <Text>CoffeeListScreen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
