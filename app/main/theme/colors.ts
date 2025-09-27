656e0b02b5c0317529dea2451d6bf86dc6eb53f2
exec zsh <wind10cloud@gmail.com>
2025-09-27 18:16:02 +0900

    Added icons and colors.ts

---
+++ app/main/theme/colors.ts
@@ -0,0 +1,8 @@
+export const colors = {
+  primary: '#3B0D0C', //ダークロースト
+  secondary: '#6A1B1A',  //焙煎赤
+  primary_light: '#A23E48', //赤ワイン／チェリー
+  accent: '#E85D04', //焙煎の炎の差し色
+}
+
+export type Colors = typeof colors;
